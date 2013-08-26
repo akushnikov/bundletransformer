437fc0bd692f2121fe64d7f47a935daa5e8337e5
Andrey Taritsyn <taritsyn@gmail.com>
2013-08-26 21:22:09 +0400

    Version 1.7.27.0: 1. In BundleTransformer.Core: 	1.1. From the Web.config file was removed the \configuration\bundleTransformer\core\assetHandler configuration element; 	1.2. In the base debugging HTTP handler added support of the ETag HTTP header; 2. In BundleTransformer.Less and BundleTransformer.LessLite fixed bug #49 "Commented-out @import directive in less file is still imported"; 3. In BundleTransformer.SassAndScss: 	3.1. Added support of Sass version 3.2.10; 	3.2. In configuration settings of Sass- and SCSS-translator added 3 new properties: LineNumbers, TraceSelectors and DebugInfo; 4. In BundleTransformer.TypeScript: 	4.1. Added support of TypeScript version 0.9.1.1; 	4.2. In configuration settings of TypeScript-translator added new property - AllowBool.

---
+++ BundleTransformer.TypeScript/Helpers/FileExtensionHelper.cs
@@ -0,0 +1,36 @@
+﻿namespace BundleTransformer.TypeScript.Helpers
+{
+	using System;
+
+	using Constants;
+
+	/// <summary>
+	/// File extension helpers
+	/// </summary>
+	internal static class FileExtensionHelpers
+	{
+		/// <summary>
+		/// Checks whether specified file extension is the JavaScript file extension 
+		/// </summary>
+		/// <param name="fileExtension">File extension</param>
+		/// <returns>Result of check</returns>
+		public static bool IsJavaScript(string fileExtension)
+		{
+			bool result = string.Equals(fileExtension, FileExtension.JavaScript, StringComparison.OrdinalIgnoreCase);
+
+			return result;
+		}
+
+		/// <summary>
+		/// Checks whether specified file extension is the TypeScript file extension 
+		/// </summary>
+		/// <param name="fileExtension">File extension</param>
+		/// <returns>Result of check</returns>
+		public static bool IsTypeScript(string fileExtension)
+		{
+			bool result = string.Equals(fileExtension, FileExtension.TypeScript, StringComparison.OrdinalIgnoreCase);
+
+			return result;
+		}
+	}
+}
