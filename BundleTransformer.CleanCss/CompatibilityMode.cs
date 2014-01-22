9b959a6ccbb89cb9bc2a34f240915159fc49c92d
Andrey Taritsyn <taritsyn@gmail.com>
2014-01-22 20:10:50 +0400

    Version 1.8.18.0: 1. In BundleTransformer.Core fixed bug #68 "Some url strings in CSS are not resolved"; 2. In BundleTransformer.Less added support of LESS version 1.6.1; 3. In BundleTransformer.SassAndScss: 	3.1. Added support of Sass version 3.2.13; 	3.2. Fixed bug #67 "BundleTransformer.SassAndScss 1.8.14: debug info line numbers in wrong format"; 4. In BundleTransformer.CoffeeScript updated CoffeeScript to commit 21db08a23d; 5. In BundleTransformer.MicrosoftAjax added support of the Microsoft Ajax Minifier version 5.7; 6. In BundleTransformer.UglifyJs: 	6.1. Added support of UglifyJS version 2.4.11; 	6.2. In compression settings added 2 new properties: `DropConsole` and `Angular`; 7. In BundleTransformer.CleanCss: 	7.1. Added support of Clean-css version 2.0.7; 	7.2. From configuration settings of CSS-minifier removed one property - `SelectorsMergeMode`, and added one new property - `Compatibility`.

---
+++ BundleTransformer.CleanCss/CompatibilityMode.cs
@@ -0,0 +1,18 @@
+﻿namespace BundleTransformer.CleanCss
+{
+	/// <summary>
+	/// Compatibility mode
+	/// </summary>
+	public enum CompatibilityMode
+	{
+		/// <summary>
+		/// None
+		/// </summary>
+		None = 0,
+
+		/// <summary>
+		/// IE8 compatibility mode
+		/// </summary>
+		Ie8
+	}
+}
